4c8f3160a5f87b156682e6ab89a1d9f40ea0d9e7
mutuajoseph <mutuajoseph17@gmail.com>
2020-09-01 00:23:13 +0300

    implemented firebase utils, achieved storing users infor after signing up

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import Homepage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUp from './pages/signInAndSignup/signInAndSignUp.component';
-import { auth  } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument  } from './firebase/firebase.utils';
 
 
 class App extends React.Component {
@@ -23,8 +23,21 @@ class App extends React.Component {
   unsubscribeFromAuth = null
   
   componentDidMount(){
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user})
+    this.unsubscribeFromAuth = auth.onAuthStateChanged( async userAuth => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapShot => {
+          this.setState({
+            currentUser: {
+              id: snapShot.id,
+              ...snapShot.data()
+            }
+          })
+        })
+        
+      }
+      this.setState({currentUser: userAuth})
     })
   }
 
--- src/pages/signInAndSignup/signInAndSignUp.component.jsx
+++ src/pages/signInAndSignup/signInAndSignUp.component.jsx
@@ -3,11 +3,13 @@ import React from 'react';
 import './signInAndSignUp.styles.scss';
 
 import SignIn from '../../components/signIn/signIn.component'
+import SignUp from '../../components/signUp/signup.component'
 
 function signInAndSignUp() {
     return (
-        <div>
+        <div className="sign-in-and-sign-up">
            <SignIn />
+           <SignUp />
         </div>
     )
 }
